a2d41a977d875a3c348b26930a5600041aa54c08
AsHim Shrestha <ashimsth89@gmail.com>
2024-11-10 12:14:00 +0545

    feat: persist store data

--- lib/store.ts
+++ lib/store.ts
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { v4 as uuid } from "uuid";
+import { persist } from "zustand/middleware";
 
 export type Status = "DONE" | "IN_PROGRESS" | "TODO";
 
@@ -22,34 +23,39 @@ export type Actions = {
   dragTask: (id: string | null) => void;
 };
 
-export const useTaskStore = create<State & Actions>((set) => ({
-  tasks: [],
-  draggedTask: null,
-  dragTask: (id: string | null) => {
-    set({
-      draggedTask: id,
-    });
-  },
-  addTask: (title: string, description?: string) =>
-    set((state) => ({
-      tasks: [
-        ...state.tasks,
-        {
-          id: uuid(),
-          title,
-          description,
-          status: "TODO",
-        },
-      ],
-    })),
-
-  removeTask: (id: string) =>
-    set((state) => ({
-      tasks: state.tasks.filter((task) => task.id !== id),
-    })),
-
-  updateTask: (id: string, status: Status) =>
-    set((state) => ({
-      tasks: state.tasks.map((task) => (task.id === id ? { ...task, status } : task)),
-    })),
-}));
+export const useTaskStore = create<State & Actions>()(
+  persist(
+    (set) => ({
+      tasks: [],
+      draggedTask: null,
+      dragTask: (id: string | null) => {
+        set({
+          draggedTask: id,
+        });
+      },
+      addTask: (title: string, description?: string) =>
+        set((state) => ({
+          tasks: [
+            ...state.tasks,
+            {
+              id: uuid(),
+              title,
+              description,
+              status: "TODO",
+            },
+          ],
+        })),
+      removeTask: (id: string) =>
+        set((state) => ({
+          tasks: state.tasks.filter((task) => task.id !== id),
+        })),
+      updateTask: (id: string, status: Status) =>
+        set((state) => ({
+          tasks: state.tasks.map((task) => (task.id === id ? { ...task, status } : task)),
+        })),
+    }),
+    {
+      name: "task",
+    }
+  )
+);
